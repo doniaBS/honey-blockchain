ebfc84e7f0ed780aaec8821c4eef4f4b04a6a6ab
donia ben sedrine <bensedrinedonia@gmail.com>
2024-04-03 14:20:43 +0100

    added generate identifier function

--- contracts/StoreHashContract.sol
+++ contracts/StoreHashContract.sol
@@ -15,6 +15,12 @@ contract HashStorage {
         owner = msg.sender;
     }
 
+     // Function to generate an identifier based on timestamp and transaction hash
+    function generateIdentifier(bytes32 transactionHash) private view returns (uint256) {
+        uint256 timestamp = block.timestamp; // Get current timestamp
+        return uint256(keccak256(abi.encodePacked(timestamp, transactionHash))); // Combine timestamp and transaction hash
+    }
+
     // Function to store an IPFS hash
     function storeIPFSHash(bytes32 ipfsHash) external {
         require(ipfsHash.length > 0, "IPFS hash cannot be empty");
