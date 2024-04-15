045d5a3e2ac9c8af7cd239c00ccc06e53a583ec0
donia ben sedrine <bensedrinedonia@gmail.com>
2024-04-16 00:20:31 +0100

    send the beekeeper address to smart contracts

--- src/login.js
+++ src/login.js
@@ -1,4 +1,3 @@
-// Include Web3.js library
 const loginForm = document.getElementById('login-form');
 const loginButton = document.getElementById('signin');
 
@@ -15,7 +14,28 @@ loginButton.addEventListener('click', async (event) => {
     const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
     const currentAccount = accounts[0]; // Get the first account from the array
     if (currentAccount) {
-      console.log('Beekeeper address (latest):', currentAccount);
+      console.log('Beekeeper address (latest):', currentAccount); // dispaly the beekeeper address from metamask
+
+      const hashStorageJSON = await $.getJSON('HashStorage.json');
+      const hashStorage = TruffleContract(hashStorageJSON);
+      hashStorage.setProvider(window.ethereum);
+
+      // Instantiate BeekeeperContract
+      const beekeeperContractJSON = await $.getJSON('BeekeeperContract.json');
+      const beekeeperContract = TruffleContract(beekeeperContractJSON);
+      beekeeperContract.setProvider(window.ethereum);
+
+      // Send beekeeper address to HashStorage contract
+      await hashStorage.deployed().then(instance => {
+        return instance.storeBeekeeperAddress(currentAccount, { from: currentAccount });
+      });
+      console.log('Beekeeper address stored in HashStorage contract:', currentAccount);
+
+      // Send beekeeper address to BeekeeperContract
+      await beekeeperContract.deployed().then(instance => {
+        return instance.registerBeekeeperWithAddress(currentAccount, { from: currentAccount });
+      });
+      console.log('Beekeeper address registered in BeekeeperContract:', currentAccount);
    } else {
       console.log('No account connected to MetaMask.');
     }
